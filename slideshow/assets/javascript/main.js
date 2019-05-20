8446dff6e94561d55af43535a41e88713d5b6dae
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-05-20 06:03:48 -0400

    added web page show

--- slideshow/assets/javascript/main.js
+++ slideshow/assets/javascript/main.js
@@ -10,20 +10,27 @@ const slideshow = {
   // A nextPhoto() function that moves currentPhotoIndex to the next index if there is one, and: 4. logs the current photo name. 4. Otherwise, log "End of slideshow";
   nextPhoto() {
     const next = this.currentPhotoIndex + 1;
-    this.photoList[next] ? console.log(this.photoList[this.currentPhotoIndex = next]) : 
-    console.log('End of slideshow');
+    this.photoList[next] ? this.log(undefined, this.photoList[this.currentPhotoIndex = next]) : 
+    this.log('End of slideshow');
   },
 
 // A prevPhoto() function that does the same thing, but backwards.
   prevPhoto() {
     const prev = this.currentPhotoIndex -1;
-    this.photoList[prev] ? console.log(this.photoList[this.currentPhotoIndex = prev]) : 
-    console.log('End of slideshow');
+    this.photoList[prev] ? this.log(undefined, this.photoList[this.currentPhotoIndex = prev]) : 
+    this.log('End of slideshow');
   },
 
 // A function getCurrentPhoto() that returns the current photo from the list.
   getCurrentPhoto() {
     return this.photoList[this.currentPhotoIndex];
+  },
+
+// Targeting the DOM
+  seconds: 0,
+  log(text, img) {
+    setTimeout((function() {console.log(text ? text: img); if (img) {document.getElementById('slideshow').innerHTML = `<img src='${img}'>`}}), this.seconds * 2000);
+    this.seconds++;
   }
 }
 
