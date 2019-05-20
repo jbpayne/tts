cb58c7c0b65a4aa6adc3c7b9469e834ead2f1f9c
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-05-20 10:01:39 -0400

    setinterval slideshow

--- slideshow/assets/javascript/main.js
+++ slideshow/assets/javascript/main.js
@@ -1,55 +1,52 @@
 // A single object named slideshow that represents the data and functionality of a picture slideshow.
 const slideshow = {
 
-// An array called photoList that contains the names of the photos as strings
-  photoList: ['https://placekitten.com/200/300', 'https://placekitten.com/100/150', 'https://placekitten.com/400/600'],
+  // An array called photoList that contains the names of the photos as strings
+  photoList: ['https://placekitten.com/200/300', 'https://placekitten.com/100/150', 'https://placekitten.com/400/600', 'https://placekitten.com/400/400', 'https://placekitten.com/300/300', 'https://placekitten.com/200/200'],
 
   // An integer currentPhotoIndex that represents which photo in the photoList is currently displayed
   currentPhotoIndex: 0,
 
+  //An empty property named playInterval
+  playInterval: '',
+
   // A nextPhoto() function that moves currentPhotoIndex to the next index if there is one, and: 4. logs the current photo name. 4. Otherwise, log "End of slideshow";
   nextPhoto() {
     const next = this.currentPhotoIndex + 1;
-    this.photoList[next] ? this.log(undefined, this.photoList[this.currentPhotoIndex = next]) : 
-    this.log('End of slideshow');
+    this.photoList[next] ? this.log(this.photoList[this.currentPhotoIndex = next]) :
+    this.pause();
   },
 
-// A prevPhoto() function that does the same thing, but backwards.
+  // A prevPhoto() function that does the same thing, but backwards.
   prevPhoto() {
-    const prev = this.currentPhotoIndex -1;
-    this.photoList[prev] ? this.log(undefined, this.photoList[this.currentPhotoIndex = prev]) : 
-    this.log('End of slideshow');
+    const prev = this.currentPhotoIndex - 1;
+    this.photoList[prev] ? this.log(this.photoList[this.currentPhotoIndex = prev]) :
+    this.pause();
   },
 
-// A function getCurrentPhoto() that returns the current photo from the list.
+  // A function getCurrentPhoto() that returns the current photo from the list.
   getCurrentPhoto() {
     return this.photoList[this.currentPhotoIndex];
   },
 
-// Targeting the DOM
-  seconds: 0,
-  log(text, img) {
-    setTimeout((function() {console.log(text ? text: img); if (img) {document.getElementById('slideshow').innerHTML = `<img src='${img}'>`}}), this.seconds * 2000);
-    this.seconds++;
-  }
+  // A pause() function that stops the slideshow (automatically if it gets to the end)
+  pause() {
+    clearInterval(this.playInterval);
+    console.log('End of slideshow');
+  },
+
+  // Targeting the DOM
+  log(img) {
+    console.log(img);
+  // Comment the line below if running in node
+    document.getElementById('slideshow').innerHTML = `<img src='${img}'>`
+  },
+
+  // A play() function that moves to the next photo every 2000ms until the end.
+  play() {
+    this.log(this.photoList[this.currentPhotoIndex]);
+    this.playInterval = setInterval(this.nextPhoto.bind(this), 2000);
+  },
 }
 
-console.log("\ngetCurrantphoto = \n" + slideshow.getCurrentPhoto());
-console.log("\nnextPhoto = ")
-slideshow.nextPhoto();
-console.log("\ngetCurrantphoto = \n" + slideshow.getCurrentPhoto());
-console.log("\nnextPhoto = ")
-slideshow.nextPhoto();
-console.log("\ngetCurrantphoto = \n" + slideshow.getCurrentPhoto());
-console.log("\nnextPhoto = ")
-slideshow.nextPhoto();
-console.log("\ngetCurrantphoto = \n" + slideshow.getCurrentPhoto());
-console.log("\nprevPhoto = ")
-slideshow.prevPhoto();
-console.log("\ngetCurrantphoto = \n" + slideshow.getCurrentPhoto());
-console.log("\nprevPhoto = ")
-slideshow.prevPhoto();
-console.log("\ngetCurrantphoto = \n" + slideshow.getCurrentPhoto());
-console.log("\nprevPhoto = ")
-slideshow.prevPhoto();
-console.log("\ngetCurrantphoto = \n" + slideshow.getCurrentPhoto());
+slideshow.play()
